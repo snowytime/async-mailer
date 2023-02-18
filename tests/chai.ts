a2c574cd33a6c31e7fa43716dd807882d77147c8
Snaer <87044139+snowytime@users.noreply.github.com>
2023-02-17 19:29:22 -0700

    ⚡️ auto patch

--- tests/chai.ts
+++
@@ -1,7 +0,0 @@
-import chai from "chai";
-import chaiAsPromised from "chai-as-promised";
-
-export const mochaGlobalSetup = () => {
-    chai.use(chaiAsPromised);
-    chai.config.includeStack = true;
-};
